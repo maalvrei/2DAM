fffc7f0b7a70eea96607c16116dea52bb6517213
maalvrei <miguemorisco@gmail.com>
2023-11-28 18:08:06 +0100

    retocado ejercicio examen

--- programacion/examenAccesoADatos/src/examen/Carta.java
+++ programacion/examenAccesoADatos/src/examen/Carta.java
@@ -1,27 +1,49 @@
 package examen;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Carta {
 
+	boolean esComodin;
 	String valor;
 	String palo;
-	boolean esComodin;
-	Random r = new Random();
+	static Random r = new Random();
+	static char [] valores = {'A' , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 'J' , 'Q' , 'K'};
+	static String [] palos = {"Picas", "Corazones", "Treboles", "Diamantes"};
 	
-	char [] valores = {'A' , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 'J' , 'Q' , 'K'};
-	String [] palos = {"Picas", "Corazones", "Treboles", "Diamantes"};
-	
-	public Carta() {
-		esComodin = r.nextBoolean();
-		if(!esComodin) {
-			char valorChar =  valores[r.nextInt(0,valores.length)];
-			if (valorChar == 'A' || valorChar == 'J' || valorChar == 'Q' || valorChar == 'K')
-				valor = String.valueOf(valorChar);
-			else
-				valor = String.valueOf((int)valorChar);
-			palo = palos[r.nextInt(0, palos.length)];
+	public Carta(String valor, String palo, boolean esComodin) {
+		if (!esComodin) {
+			this.esComodin = false;
+			this.valor = valor;
+			this.palo = palo;
+		} else {
+			this.esComodin = true;
+			this.valor = null;
+			this.palo = null;
 		}
+	}
+	
+	public static Carta cartaAleatoriaDeLaBaraja() {
+		List<Carta> listaBaraja = new ArrayList<>();
+		for (char c : valores) {
+			listaBaraja.add(new Carta(c == 'A' || c == 'J' || c == 'Q' || c == 'K'? String.valueOf(c) : String.valueOf((int)c),palos[0], false));
+		}
+		for (char c : valores) {
+			listaBaraja.add(new Carta(c == 'A' || c == 'J' || c == 'Q' || c == 'K'? String.valueOf(c) : String.valueOf((int)c),palos[1], false));
+		}
+		for (char c : valores) {
+			listaBaraja.add(new Carta(c == 'A' || c == 'J' || c == 'Q' || c == 'K'? String.valueOf(c) : String.valueOf((int)c),palos[2], false));
+		}
+		for (char c : valores) {
+			listaBaraja.add(new Carta(c == 'A' || c == 'J' || c == 'Q' || c == 'K'? String.valueOf(c) : String.valueOf((int)c),palos[3], false));
+		}
+		listaBaraja.add(new Carta(null, null, true));
+		listaBaraja.add(new Carta(null, null, true));
+		Carta[] arrayBaraja = new Carta[listaBaraja.size()];
+		arrayBaraja = listaBaraja.toArray(arrayBaraja);
+		return arrayBaraja[r.nextInt(0,arrayBaraja.length)];
 	}
 	
 	@Override
--- programacion/examenAccesoADatos/src/examen/Lanzamiento.java
+++ programacion/examenAccesoADatos/src/examen/Lanzamiento.java
@@ -3,7 +3,6 @@ package examen;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,105 +13,137 @@ import java.util.regex.Pattern;
 
 public class Lanzamiento {
 
-	private Moneda moneda;
+	private String moneda;
 	private Carta carta;
 	private int dado1;
 	private int dado2;
 	Random r = new Random();
 
-	public int getDado1() {
-		return dado1;
-	}
-
-	public int getDado2() {
-		return dado2;
-	}
-
 	public Lanzamiento() {
-		moneda = Moneda.obtieneMonedaAleatoria();
-		carta = new Carta();
+		moneda = r.nextBoolean() ? "Cara" : "Cruz";
+		carta = Carta.cartaAleatoriaDeLaBaraja();
 		dado1 = r.nextInt(1, 7);
 		dado2 = r.nextInt(1, 7);
 	}
 
-	public static void main(String[] args) {
-
-		String rutaTxt = "C:\\Users\\José\\Desktop\\repositorio\\2DAM\\programacion\\examenAccesoADatos\\src\\examen\\lanzamientos.txt";
-
-		try (BufferedWriter bw1 = new BufferedWriter(new FileWriter(rutaTxt))) {
+	public static void escribeTXT(String ruta) {
+		
+		File f = new File(ruta,"lanzamientos.txt");
+		if (!f.exists())
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+		try (BufferedWriter bw1 = new BufferedWriter(new FileWriter(f))) {
 			Lanzamiento l = new Lanzamiento();
 			boolean lanzamientoInvalido1 = (l.carta.valor != "A" && l.carta.valor != "J" && l.carta.valor != "Q"
-					&& l.carta.valor != "K") && (l.dado1 % 2 != 0 || l.dado2 % 2 != 0)
-					&& l.moneda.toString().equals("Cruz")
+					&& l.carta.valor != "K") && (l.dado1 % 2 != 0 || l.dado2 % 2 != 0) && l.moneda.equals("Cruz")
 					&& (l.carta.palo == "Treboles" && Integer.valueOf(l.carta.valor) % 2 != 0);
-			boolean lanzamientoInvalido2 = (l.getDado1() + l.getDado2() == 7) && (l.moneda.toString().equals("Cara"))
+			boolean lanzamientoInvalido2 = (l.dado1 + l.dado2 == 7) && (l.moneda.equals("Cara"))
 					&& l.carta.esComodin == false;
 			int lanzamientosValidos = 0;
 			for (int i = lanzamientosValidos; i < 10_000; i++) {
 				l = new Lanzamiento();
-				if (!lanzamientoInvalido1 && !lanzamientoInvalido2)
+				if (!lanzamientoInvalido1 && !lanzamientoInvalido2) {
 					lanzamientosValidos++;
-				bw1.append(l.carta.esComodin ? "Comodin|" + l.getDado1() + "|" + l.getDado2() + "|" + l.moneda + "\n"
-						: l.carta.valor + "|" + l.carta.palo + "|" + l.getDado1() + "|" + l.getDado2() + "|" + l.moneda
-								+ "\n");
+					bw1.append(l.carta.esComodin ? "Comodin|" + l.dado1 + "|" + l.dado2 + "|" + l.moneda + "\n"
+							: l.carta.valor + "|" + l.carta.palo + "|" + l.dado1 + "|" + l.dado2 + "|" + l.moneda
+									+ "\n");
+				}
 			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+	}
 
+	public static List<String[]> leeTXT(String ruta) {
 		List<String[]> lineas = new ArrayList<>();
 
-		try (BufferedReader br1 = new BufferedReader(new FileReader(rutaTxt))) {
+		try (BufferedReader br1 = new BufferedReader(new FileReader(ruta))) {
 			String separador = Pattern.quote("|");
 			br1.lines().forEach(s -> lineas.add(s.split(separador)));
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+		return lineas;
+	}
 
-		String rutaSQL = "C:\\Users\\José\\Desktop\\repositorio\\2DAM\\programacion\\examenAccesoADatos\\src\\examen\\lanzamientossql.txt";
-
-		try (BufferedWriter bw2 = new BufferedWriter(new FileWriter(rutaSQL))) {
-			int lineasValidasSql = 0;
+	public static void escribeSQL(String ruta, List<String[]> lineas) {
+		
+		File f = new File(ruta,"sentencias.sql");
+		if (!f.exists())
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+		try (BufferedWriter bw2 = new BufferedWriter(new FileWriter(f))) {
+			int sentenciasCreadas = 0;
 			for (int i = lineas.size() - 1; i > 0; i--) {
 				String[] linea = lineas.get(i);
-				if ((linea[1].equals("Corazones") || linea[1].equals("Picas")) && lineasValidasSql == 0) {
+				boolean lineaValida = (linea[1].equals("Corazones") || linea[1].equals("Picas"));
+				if (lineaValida && sentenciasCreadas == 0) {
 					bw2.append(String.format(
 							"INSER INTO lanzamientos ('Numero de la carta' , 'Palo', 'Dado 1', 'Dado 2', 'Moneda') VALUES ('%s','%s','%s','%s','%s'),%n",
 							linea[0], linea[1], linea[2], linea[3], linea[4]));
-					lineasValidasSql += 1;
-				} else if ((linea[1].equals("Corazones") || linea[1].equals("Picas")) && (lineasValidasSql < 200)) {
+					sentenciasCreadas += 1;
+				} else if (lineaValida && sentenciasCreadas < 200) {
 					bw2.append(String.format("('%s','%s','%s','%s','%s'),%n", linea[0], linea[1], linea[2], linea[3],
 							linea[4]));
-					lineasValidasSql += 1;
-				} else if ((linea[1].equals("Corazones") || linea[1].equals("Picas")) && (lineasValidasSql == 200)) {
+					sentenciasCreadas += 1;
+				} else if (lineaValida && sentenciasCreadas == 200) {
 					bw2.append(String.format("('%s','%s','%s','%s','%s');%n", linea[0], linea[1], linea[2], linea[3],
 							linea[4]));
-					lineasValidasSql += 1;
+					sentenciasCreadas += 1;
 				}
 			}
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
+	}
 
-		String rutaHtml = "C:\\Users\\José\\Desktop\\repositorio\\2DAM\\programacion\\examenAccesoADatos\\src\\examen\\lanzamientoshtml.html";
-
-		try (BufferedWriter bw3 = new BufferedWriter(new FileWriter(rutaHtml))) {
-			int lineasValidasHtml = 0;
+	public static void escribeHTML(String ruta, List<String[]> lineas) {
+		
+		File f = new File(ruta,"tablaLanzamientos.html");
+		if (!f.exists())
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+		try (BufferedWriter bw3 = new BufferedWriter(new FileWriter(f))) {
+			int filasCreadas = 0;
 			bw3.append(
 					"<table><tr><th>Numero de Carta</th><th>Palo</th><th>Dado 1</th><th>Dado 2</th><th>Moneda</th></tr>");
 			for (int i = lineas.size() - 1; i > 0; i--) {
 				String[] linea = lineas.get(i);
-				if ((linea[1].equals("Corazones") || linea[1].equals("Picas")) && (lineasValidasHtml < 200)) {
+				boolean lineaValida = (linea[1].equals("Corazones") || linea[1].equals("Picas"));
+				if (lineaValida && filasCreadas < 200) {
 					bw3.append(String.format("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
 							linea[0], linea[1], linea[2], linea[3], linea[4]));
-					lineasValidasHtml += 1;
+					filasCreadas += 1;
 				}
 			}
-			
 			bw3.append("</table>");
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void main(String[] args) {
+
+		String rutaTxt = "C:\\Users\\migue\\Desktop\\repo 2º dam\\2DAM\\programacion\\examenAccesoADatos\\src\\examen\\";
+		escribeTXT(rutaTxt);
+		String rutaSQL = "C:\\Users\\migue\\Desktop\\repo 2º dam\\2DAM\\programacion\\examenAccesoADatos\\src\\examen\\";
+		List<String[]> lineas = leeTXT(rutaTxt + "lanzamientos.txt");
+		escribeSQL(rutaSQL, lineas);
+		String rutaHtml = "C:\\Users\\migue\\Desktop\\repo 2º dam\\2DAM\\programacion\\examenAccesoADatos\\src\\examen\\";
+		escribeHTML(rutaHtml, lineas);
+
+
 	}
 }
