64db91d78e5e20623c8a8a070f12d2c16370efbd
maalvrei <miguemorisco@gmail.com>
2023-12-11 10:18:17 +0100

    avanzando con spring

---
+++ spring/demo02/src/main/java/com/example/demo/paquete/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.demo.paquete;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	@GetMapping({"home",""})
+	public String homeMethod() {
+		return "home";
+	}
+	
+	@GetMapping("home-aleatoria")
+	public String m(Model model) {
+		Random random = new Random();
+		int miRandom = random.nextInt();
+		model.addAttribute("miRandom", miRandom);
+		return "htmlAleatorio";
+	}
+}
