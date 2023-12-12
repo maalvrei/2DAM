1f93700591b5436480828d8dc8c73f7d4ea1a698
maalvrei <miguemorisco@gmail.com>
2023-12-12 23:21:27 +0100

    curioseando con spring

--- spring/demo02/src/main/java/com/example/demo/paquete/HomeController.java
+++ spring/demo02/src/main/java/com/example/demo/paquete/HomeController.java
@@ -1,5 +1,11 @@
 package com.example.demo.paquete;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.stereotype.Controller;
@@ -21,5 +27,41 @@ public class HomeController {
 		int miRandom = random.nextInt();
 		model.addAttribute("miRandom", miRandom);
 		return "htmlAleatorio";
+	}
+	
+	@GetMapping("formulario")
+	public String m2(Model model) {
+		List<String[]> listaConPreguntas = leeTXT();
+		String pregunta1 = devuelveLaPregunta(listaConPreguntas);
+		String pregunta2 = devuelveLaPregunta(listaConPreguntas);
+		String pregunta3 = devuelveLaPregunta(listaConPreguntas);
+		model.addAttribute("pregunta1", pregunta1);
+		model.addAttribute("pregunta2", pregunta2);
+		model.addAttribute("pregunta3", pregunta3);
+		return "preguntas";
+	}
+	
+	public static List<String[]> leeTXT() {
+		File f = new File("C:\\Users\\migue\\Desktop\\pcp.txt");
+		FileReader fr = null;
+		try {
+			fr = new FileReader(f);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		BufferedReader br = new BufferedReader(fr);
+		int tamanioArray = 0;
+		List <String[]> lineas = new ArrayList<>();
+		br.lines().forEach(s-> lineas.add(s.split("\\|")));
+		return lineas;
+	}
+	
+	public static String devuelveLaPregunta(List<String[]> lista) {
+		Random r = new Random();
+		return lista.get(r.nextInt(0,lista.size()-1))[1];
+	}
+	
+	public static void main(String[] args) {
+		leeTXT();
 	}
 }
