626300197be7eedfcf887be0df91a019cd6af113
maalvrei <miguemorisco@gmail.com>
2024-01-15 20:42:41 +0100

    avanzando en la app

--- spring/testApp/src/main/java/com/example/demo/controller/PreguntaController.java
+++ spring/testApp/src/main/java/com/example/demo/controller/PreguntaController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.model.entity.Pregunta;
 import com.example.demo.service.IPreguntaService;
 
 @Controller
@@ -27,10 +30,33 @@ public class PreguntaController {
 		return "home";
 	}
 	
-	@GetMapping("/home/modificar_pregunta")
-	public String mofidicar_pregunta (@PathVariable Long id, Model model){
-		model.addAttribute("pregunta",preguntaService.findById(id));
-		return "modificar_pregunta";
+	@GetMapping("/home/formulario")
+	public String crearPregunta (Model model) {
+		model.addAttribute("pregunta", new Pregunta());
+		return "formulario";
+	}
+	
+	@PostMapping("/home/formulario")
+	// Aquí cliente está ya poblado con los datos del formulario
+	public String guardar(Pregunta pregunta) {
+		preguntaService.save(pregunta);
+		return "redirect:listar";
+	}
+	
+	@GetMapping("/home/eliminar/{id}")
+	public String eliminar (@PathVariable Long id) {
+		preguntaService.delete(id);
+		return "redirect:/home/listar";
+	}
+	
+	@GetMapping("/home/formulario/{id}")
+	public String mofidicarPregunta (@PathVariable Long id, Model model){
+		List<Pregunta> listaDePreguntas = (List<Pregunta>) preguntaService.findAll();
+		Pregunta p = null;
+		for (Pregunta pr : listaDePreguntas)
+			if (pr.getId() == id) p = pr;
+		model.addAttribute("pregunta",p);
+		return "formulario";
 	}
 	
 
